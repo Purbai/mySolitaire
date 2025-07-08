2a65188b03f81a42ef13616c7fb1b3741489d72f
Purbai <varsani_p@hotmail.com>
2025-07-08 14:43:53 +0100

    create the card deck on start button click

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import './style.scss'
 import { SUITS, CARD_LABELS, Card, type CardLabel, type Suit } from "./card_data/card_data";
 import { createDeck } from './card_data/create_deck';
 
-const deck = createDeck(Card);
+const start = document.querySelector<HTMLButtonElement>('#start');
 
-console.log(deck);
+const handleStartClick = () => {
+    const deck = createDeck(Card);
+    console.log(deck);
+};
+
+if (start) {
+    start.addEventListener('click', handleStartClick);
+  } else {
+    alert('Start button not found in the DOM');
+  }
